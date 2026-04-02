e2beb402b22b2f0878b8ee5be25b4d164cd3de5d
Jenny <bduong@umass.edu>
2026-04-02 13:54:54 -0400

    Remove accidental ufound-app folder

--- ufound-app/src/utils/supabase.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env['REACT_APP_SUPABASE_URL'] as string
-const supabaseAnonKey = process.env['REACT_APP_SUPABASE_ANON_KEY'] as string
-
-console.log('URL:', supabaseUrl)
-console.log('KEY:', supabaseAnonKey)
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
