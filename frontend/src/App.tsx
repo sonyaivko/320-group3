d250a657644daddf5ca1a02f469cd83661eeb63d
jpinkhamumass <jpinkham@umass.edu>
2026-04-15 12:36:08 -0400

    Added protected routes so that you must login before proceding to create / view reports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,18 +5,45 @@ import Signup from "./pages/Signup";
 import ViewReports from "./pages/ViewReports";
 import CreateReport from "./pages/CreateReport";
 import Search from "./pages/Search";
+import ProtectedRoute from "./ProtectedRoute";
 import "./App.css";
 
 export default function App() {
   return (
     <Router>
       <Routes>
+        {/* PUBLIC */}
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/viewreports" element={<ViewReports />} />
-        <Route path="/createreport" element={<CreateReport />} />
-        <Route path="/search" element={<Search />} /> 
+
+        {/* PROTECTED */}
+        <Route
+          path="/viewreports"
+          element={
+            <ProtectedRoute>
+              <ViewReports />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/createreport"
+          element={
+            <ProtectedRoute>
+              <CreateReport />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/search"
+          element={
+            <ProtectedRoute>
+              <Search />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
