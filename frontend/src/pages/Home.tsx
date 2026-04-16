059442fa88d5820db0d8f8dca8c055d0a909ab51
jpinkhamumass <150302347+jpinkhamumass@users.noreply.github.com>
2026-04-15 22:09:13 -0400

    Remove token from local storage on logout

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -15,6 +15,7 @@ const Home: React.FC = () => {
 
   const handleLogout = () => {
     localStorage.removeItem("user");
+    localStorage.removeItem("token");
     setIsLoggedIn(false);
     navigate("/");
   };
